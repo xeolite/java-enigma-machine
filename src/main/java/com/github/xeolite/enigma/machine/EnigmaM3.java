359cd63d420a74999743ef05fdd267492295e43d
xeolite <xeolite@users.noreply.github.com>
2015-03-27 18:01:13 +0000

    Require a standard reflector in M3 variant

--- src/main/java/com/github/xeolite/enigma/machine/EnigmaM3.java
+++ src/main/java/com/github/xeolite/enigma/machine/EnigmaM3.java
@@ -2,6 +2,7 @@ package com.github.xeolite.enigma.machine;
 
 import com.github.xeolite.enigma.plugboard.Plugboard;
 import com.github.xeolite.enigma.reflector.Reflector;
+import com.github.xeolite.enigma.reflector.StandardReflector;
 import com.github.xeolite.enigma.rotor.RotorGroup;
 import com.github.xeolite.enigma.rotor.StandardRotor;
 import com.github.xeolite.enigma.stator.Stator;
@@ -9,7 +10,7 @@ import com.github.xeolite.enigma.step.LeverGroup;
 
 public class EnigmaM3 extends Enigma {
 
-    public EnigmaM3(Reflector reflector, StandardRotor leftRotor, StandardRotor centerRotor, StandardRotor rightRotor, Plugboard plugboard) {
+    public EnigmaM3(StandardReflector reflector, StandardRotor leftRotor, StandardRotor centerRotor, StandardRotor rightRotor, Plugboard plugboard) {
         super(new EnigmaMechanism(reflector, new RotorGroup(leftRotor, centerRotor, rightRotor), new LeverGroup(), Stator.MILITARY, plugboard));
     }
 }

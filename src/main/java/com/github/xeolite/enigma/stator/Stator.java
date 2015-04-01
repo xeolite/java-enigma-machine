5e013d3344df63387149f81dd2a6ba8d1b2da6c8
xeolite <xeolite@users.noreply.github.com>
2015-04-01 10:11:31 -0700

    Correction of commercial stator

--- src/main/java/com/github/xeolite/enigma/stator/Stator.java
+++ src/main/java/com/github/xeolite/enigma/stator/Stator.java
@@ -6,7 +6,7 @@ import com.github.xeolite.enigma.cipher.SubstitutionCipher;
 public class Stator {
 
     public static final Stator MILITARY = new Stator(new SubstitutionCipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-    public static final Stator COMMERCIAL = new Stator(Cipher.inverse(new SubstitutionCipher("QWERTYUIOPASDFGHJKLZXCVBNM")));
+    public static final Stator COMMERCIAL = new Stator(Cipher.inverse(new SubstitutionCipher("QWERTZUIOASDFGHJKPYXCVBNML")));
 
     private Cipher inboundCipher;
     private Cipher outboundCipher;
